ccdf3933a8f4f5239920f0c34355b55d76f0a511
Pooja Charla <poojareddycharla@gmail.com>
2018-08-01 10:39:35 -0600

    password hashing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ def create
 
   #render plain: params[:article].inspect
   @article = Article.new(article_params)
+  #@article.user= User.first
   if @article.save
     flash[:success]="Article was successfully created"
     redirect_to article_path(@article)
