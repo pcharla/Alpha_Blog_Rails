b78b3614683d2ecd4eada758b3402d23f71843da
Pooja Charla <poojareddycharla@gmail.com>
2018-07-31 22:44:53 -0600

    Styling continued

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:edit, :update, :show, :destroy]
-  
+
   def index
     @articles = Article.all
   end
@@ -18,7 +18,7 @@ def create
   #render plain: params[:article].inspect
   @article = Article.new(article_params)
   if @article.save
-    flash[:notice]="Article was successfully created"
+    flash[:success]="Article was successfully created"
     redirect_to article_path(@article)
   else
     render 'new'
@@ -28,7 +28,7 @@ end
 def update
 
   if @article.update(article_params)
-    flash[:notice]= "Article was successfully updated"
+    flash[:success]= "Article was successfully updated"
     redirect_to article_path(@article)
   else
     render 'edit'
@@ -42,7 +42,7 @@ end
 def destroy
   @article= Article.find(params[:id])
   @article.destroy
-  flash[:notice]= "Article was successfully deleted"
+  flash[:danger]= "Article was successfully deleted"
   redirect_to articles_path
 
 end
